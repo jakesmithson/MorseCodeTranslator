e74aa14802aefb0359c949da004730e472d1e6bb
Jake Smithson <jssmithson@yahoo.com>
2019-09-11 23:58:18 -0400

    Add files via upload

---
+++ MorseCodeTranslator.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+/**
+ * MorseCodeTranslator Translates a users string from either morse code to
+ * english or vice versa.
+ * 
+ * @author Jake Smithson
+ */
+public class MorseCodeTranslator {
+    public static final String[] ALPHA = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
+    public static final String[] MORSE = "*- -*** -*-* -** * **-* --* **** ** *--- -*- *-** -- -* --- *--* --*- *-* *** - **- ***- *-- -**- -*-- --**".split(" ");
+    public static Scanner in = new Scanner(System.in);
+
+    /**
+     * Main method for the MorseCodeTranslator class, asks the user what language they want to translate,
+     * then asks for a string to be translated. Prints the translated string in the opposite language.
+     * @param args command line args (not used)
+     */
+    public static void main(String[] args) {
+        HashMap<String, String> ALPHA_MORSE = new HashMap<String, String>();
+        for(int i = 0; i < ALPHA.length; i++) {
+            ALPHA_MORSE.put(ALPHA[i], MORSE[i]);
+        }
+        HashMap<String, String> MORSE_ALPHA = new HashMap<String, String>();
+        for(int i = 0; i < MORSE.length; i++) {
+            MORSE_ALPHA.put(MORSE[i], ALPHA[i]);
+        }
+
+        //englishToMorse("I sure hope this works", ALPHA_MORSE);
+        //morseToEnglish("**       ***   **-   *-*   *       ****   ---   *--*   *       -   ****   **   ***       *--   ---   *-*   -*-   ***", MORSE_ALPHA);
+    }
+
+    /**
+     * printHashMap method prints a given hashmap in the format of key : value.
+     * Purely for testing purposes. Remove when first final build is finished.
+     * @param map is the HashMap to be printed.
+     */
+    public static void printHashMap(HashMap<String, String> map) {
+        for(String key: map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+    }
+
+    /**
+     * englishToMorse is the process in which a string is converted from English to Morse code.
+     * This is done by comparing a letter in english and fetching its matching morse code value
+     * by using a hashmap.
+     * @param str is the String that is being translated.
+     */
+    public static void englishToMorse(String str, HashMap<String, String> map) {
+        String result = "";
+        str = str.trim();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.substring(i,i+1).equals(" ")) {
+                result += "    ";
+            }
+            else {
+                result += map.get(str.substring(i,i+1).toLowerCase()) + "   ";
+            }
+        }
+        System.out.println("Your translated result is:\n" + result);
+    }
+
+    /** ----------- WIP
+     * morseToEnglish is the process in which a string is converted from Morse Code to English.
+     * This is done by comparing a letter in morse code and fetching its matching english value
+     * by using a hashmap.
+     * @param str is the String that is being translated.
+     */
+    public static void morseToEnglish(String str, HashMap<String, String> map) {
+        String result = "";
+        str = str.trim();
+        for(int i = 0; i < str.length(); i++) {
+            if(i + 6 < str.length() && str.substring(i,i+6).equals("       ")) {
+                result += " ";
+                i += 6;
+            }
+            else if(i + 2 < str.length() && str.substring(i,i+2).equals("   ")) {
+                i += 2;
+            }
+            else if(str.indexOf(" ", i) != -1) {
+                result += map.get(str.substring(i,str.indexOf(" ", i))) + "   ";
+                i = str.indexOf(" ");
+            }
+            else {
+                result += map.get(str.substring(i));
+                i = str.length();
+            }
+        }
+        System.out.println("Your translated result is:\n" + result);
+    }
+}
