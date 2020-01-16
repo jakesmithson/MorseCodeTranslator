151eb279094c1584ce721bffb09aaed70a7f5a30
Jake Smithson <jssmithson@yahoo.com>
2020-01-16 15:43:52 -0500

    Add files via upload
    Greatly improved the efficiency of morseToEnglish method, removed methods that weren't needed

--- MorseCodeTranslator.java
+++ MorseCodeTranslator.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashMap;
 
 /**
  * MorseCodeTranslator Translates a users string from either morse code to
@@ -18,7 +19,7 @@ public class MorseCodeTranslator {
      * then asks for a string to be translated. Prints the translated string in the opposite language.
      * @param args command line args (not used)
      */
-public static void main(String[] args) {
+    public static void main(String[] args) {
         HashMap<String, String> ALPHA_MORSE = new HashMap<String, String>();
         for(int i = 0; i < ALPHA.length; i++) {
             ALPHA_MORSE.put(ALPHA[i], MORSE[i]);
@@ -27,20 +28,9 @@ public static void main(String[] args) {
         for(int i = 0; i < MORSE.length; i++) {
             MORSE_ALPHA.put(MORSE[i], ALPHA[i]);
         }
-        //englishToMorse("a bc", ALPHA_MORSE);
-        morseToEnglishCorrect("*-   -***   -*-*   -**       *   **-*", MORSE_ALPHA);
-        //morseToEnglish("*-   -***   -*-*   -**       *   **-*", MORSE_ALPHA);
-    }
+        englishToMorse("abcd ef", ALPHA_MORSE);
+        morseToEnglish("*-   -***   -*-*   -**       *   **-*", MORSE_ALPHA);
 
-    /**
-     * printHashMap method prints a given hashmap in the format of key : value.
-     * Purely for testing purposes. Remove when first final build is finished.
-     * @param map is the HashMap to be printed.
-     */
-    public static void printHashMap(HashMap<String, String> map) {
-        for(String key: map.keySet()) {
-            System.out.println(key + " : " + map.get(key));
-        }
     }
 
     /**
@@ -63,7 +53,7 @@ public static void main(String[] args) {
         System.out.println("Your translated result is:\n" + result);
     }
 
-    /** ----------- WIP
+    /**
      * morseToEnglish is the process in which a string is converted from Morse Code to English.
      * This is done by comparing a letter in morse code and fetching its matching english value
      * by using a hashmap.
@@ -71,72 +61,19 @@ public static void main(String[] args) {
      */
     public static void morseToEnglish(String str, HashMap<String, String> map) {
         String result = "";
-        str = str.trim();
-        for(int i = 0; i < str.length(); i++) {
-            System.out.println(i);
-            if(i + 6 < str.length() && str.substring(i,i+6).equals("       ")) {
-                result += " ";
-                i += 6;
+        Scanner line = new Scanner(str);
+        line.useDelimiter("       ");
+        while(line.hasNext()) {
+            Scanner word = new Scanner(line.next());
+            word.useDelimiter("   ");
+            while(word.hasNext()) {
+                result += map.get(word.next());
             }
-            else if(i + 2 < str.length() && str.substring(i,i+2).equals("   ")) {
-                i += 2;
-            }
-            else if(str.indexOf(" ", i) != -1) {
-                result += map.get(str.substring(i,str.indexOf(" ", i))) + "   ";
-                i = str.indexOf(" ");
-            }
-            else {
-                result += map.get(str.substring(i));
-                i = str.length();
-            }
+            result += " ";
+            word.close();
         }
-        System.out.println("Your translated result is:\n" + result);
-    }
-
-    public static void morseToEnglishCorrect(String str, HashMap<String, String> map) {
-        String result = "";
-        String buf = "";
-        int i = 0;
-        for(i = 0; i < str.length(); i++) {
-            buf += str.substring(i, i + 1);
-            //System.out.println(buf);
-            if(buf.endsWith(" ")) {
-                //System.out.println("|"+buf+"|");
-                buf = buf.trim();
-                if(map.get(buf) != null) {
-                    result += map.get(buf);
-                    buf = "";
-                    result += parseSpaces(str, i);
-                    if(result.endsWith(" ")) {
-                        i += 6;
-                    }
-                    else {
-                        i += 2;
-                    }
-                }
-            }
-        }
-        // Don't forget to check map
-        // if buf contains text
-        if (!(buf.equals(""))) {
-            String s = map.get(buf);
-            if (s != null) {
-                result += s;
-            }
-        }
+        line.close();
         System.out.println(result);
     }
 
-    public static String parseSpaces(String str, int index) {
-        if(index == str.length()) {
-            return "";
-        }
-        //System.out.println(str.substring(index, index + 7));
-        if(str.substring(index, index + 7).equals("       ")) {
-            return " ";
-        }
-        else {
-            return "";
-        }
-    }
 }
